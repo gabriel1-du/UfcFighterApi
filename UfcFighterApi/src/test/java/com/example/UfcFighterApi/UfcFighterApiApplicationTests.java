9c9a7767aaed0076a41c921e63ecfd70e020d5f7
Gabriel Duran <gabrieldurans999@gmail.com>
2025-05-09 09:57:49 -0400

    First Commit : UfcFighterApi / SptingBoot / Java

---
+++ UfcFighterApi/src/test/java/com/example/UfcFighterApi/UfcFighterApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.UfcFighterApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UfcFighterApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
